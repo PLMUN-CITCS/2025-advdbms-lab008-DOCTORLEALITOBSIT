8bc35c8bcc446a0b6043de0e9bf62b1c7d952997
DOCTORLEALITOBSIT <doctorlealito_bsit@plmun.edu.ph>
2025-02-16 19:09:30 +0800

    Update insert_bookstore_data.sql

--- bookstore_database/insert_bookstore_data.sql
+++ bookstore_database/insert_bookstore_data.sql
@@ -1,3 +1,5 @@
+USE `BookstoreDB`;
+
 INSERT INTO `Books` (`ISBN`, `Title`, `Price`) VALUES
 ('9781234567890', 'The Hitchhiker''s Guide to the Galaxy', 7.99),
 ('9780321765723', 'The Lord of the Rings', 12.99),
